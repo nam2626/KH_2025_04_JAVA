bec4da7be23bf7f81006aaf63619e5b7861268b0
nam2626 <nam2626@naver.com>
2025-04-02 14:57:41 +0900

    단축키 및 출력 추가

--- Step01/src/E01_HelloWorld.java
+++ Step01/src/E01_HelloWorld.java
@@ -3,5 +3,9 @@ public class E01_HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("남상규");
+		System.out.println(100);
+		System.out.println(100+50);
+		System.out.println("100"+50);
+		System.out.println("100"+50+40);
 	}
 }
