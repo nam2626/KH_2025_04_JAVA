14a5b266a80a7930318030be5b3603244dc5d2c1
nam2626 <nam2626@naver.com>
2025-04-02 19:13:12 +0900

    단축키 및 변수 내용 추가

--- Step01/src/E02_PrintMain.java
+++ Step01/src/E02_PrintMain.java
@@ -27,6 +27,19 @@ public class E02_PrintMain {
 		 * System.out.println(1); System.out.println(2); System.out.println(3);
 		 * System.out.println(4);
 		 */
+		
+		System.out.print("-----------------\n"); // \n 줄바꿈 문자
+		System.out.print("Hello");
+		System.out.print("World\n");
+		System.out.print("---------\n\n--------\n");
+		System.out.println("\\n"); // \\ 역슬래시 한개로 출력
+		System.out.println("\""); // \" 큰따옴표 출력
+		System.out.println("1\t2"); // 탭사이즈 만큼 띄워쓰기
+		
+		//         숫자     숫자
+		//안녕하세요9282갑시다2234
+		System.out.println("안녕하세요"+9282+"갑시다"+2234);
+		System.out.printf("안녕하세요%d갑시다%d",9282,2234);
 		 
 	}
 
---
+++ Step01/src/E03_VariableTest.java
@@ -0,0 +1,21 @@
+
+public class E03_VariableTest {
+	/*
+	 * 변수(Variable) : 데이터 하나를 저장할 공간
+	 * 
+	 */
+	public static void main(String[] args) {
+		//변수 선언 부분 --> 정수하나 저장할 공간을 만들겠다.
+		int num;
+		//변수값 초기화(initialize) --> 해당 변수에 값을 저장
+		num = 100;
+		num = 50; // 기존값을 없어지고 새 값이 저장
+		//num에 저장된 값을 읽어온 후에 출력
+		System.out.println(num);
+		
+		int age = 20; //변수를 선언 후에 바로 초기화까지 하겠다.
+		System.out.println(age);
+		
+	}
+
+}
