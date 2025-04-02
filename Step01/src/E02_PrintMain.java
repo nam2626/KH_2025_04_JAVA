de61684a14e23c0621bcf5d2e30dfe226751ffbe
nam2626 <nam2626@naver.com>
2025-04-02 15:35:54 +0900

    단축키 및 주석  추가

---
+++ Step01/src/E02_PrintMain.java
@@ -0,0 +1,38 @@
+
+public class E02_PrintMain {
+	/*
+	 * 여러줄 주석 
+	 */
+	
+	//한줄 단위 주석
+	
+	/*
+	 * 	주석
+	 * 		코드에 설명문을 작성하는 영역
+	 * 		주석은 실행이 안되는 코드
+	 * 
+	 * 
+	 */
+	public static void main(String[] args) {
+		System.out.println(10);
+		//System.out.println(10 + 5);
+		System.out.println(10 - 5);
+		
+		System.out.println("Hello");
+		System.out.println("Hello" + "World");
+		System.out.println("Hello" + 10 + 20);	
+		
+		
+		/*
+		 * System.out.println(1); System.out.println(2); System.out.println(3);
+		 * System.out.println(4);
+		 */
+		 
+	}
+
+}
+
+
+
+
+
