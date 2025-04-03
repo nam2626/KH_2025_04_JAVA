38c5da41664594c80cd96dda64bac93787bedaa3
nam2626 <nam2626@naver.com>
2025-04-03 14:55:34 +0900

    형변환

--- Step02/src/E04_Operator.java
+++ Step02/src/E04_Operator.java
@@ -21,6 +21,15 @@ public class E04_Operator {
 		System.out.println(n1 % n2);
 		
 		//실수형 변수 두개 선언해서 초기화 후 사칙연산 결과 출력
+		double d1 = 10.2, d2 = 3.3;
+		
+		System.out.println(d1 + d2);
+		System.out.println(d1 - d2);
+		System.out.println(d1 * d2);
+		System.out.println(d1 / d2);
+		System.out.println(d1 % d2);
+		
+		System.out.println('A' + 1);
 		
 	}
 
