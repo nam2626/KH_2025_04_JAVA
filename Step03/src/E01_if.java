b84d223d2b41b0f427da55be615278490249473f
nam2626 <nam2626@naver.com>
2025-04-07 10:41:34 +0900

    if문

---
+++ Step03/src/E01_if.java
@@ -0,0 +1,36 @@
+
+public class E01_if {
+	/*
+	 * 	if문
+	 * 		조건식이 true면 if문에 붙어있는 { 실행영역 }에 있는 코드를 실행
+	 * 		조건식이 false면 해당 if문을 건너뜀
+	 * 
+	 *  조건식 : 관계연산자와 논리연산자를 이용한 수식, 결과는 true/false로 나옴
+	 *  
+	 *  if문의 구성요소는 if, else if, else가 있음	
+	 * 
+	 * 	if(조건식){
+	 * 		실행할 코드...
+	 * 		실행할 코드...
+	 * 		실행할 코드...
+	 * 	}
+	 * 
+	 */
+	public static void main(String[] args) {
+		int n = -10;
+		
+		if(n > 0) {
+			System.out.println("양수입니다.");
+		}
+		
+		if(n < 0) {
+			System.out.println("음수입니다.");
+		}
+		
+		System.out.println("프로그램 종료");
+	}
+
+}
+
+
+
