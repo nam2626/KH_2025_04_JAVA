2b578d0847b141c9909a9c7e2dabf9a1d856a7f2
nam2626 <nam2626@naver.com>
2025-04-07 14:56:58 +0900

    if - else 예제

---
+++ Step03/src/E07_Quest.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class E07_Quest {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("첫번째 숫자 입력 : ");
+		int n1 = sc.nextInt();
+		System.out.println("두번째 숫자 입력 : ");
+		int n2 = sc.nextInt();
+
+		// 두 숫자 중에 큰숫자, 작은 숫자 구분해서 출력
+		// 큰 숫자 : ?, 작은 숫자 : ?
+		
+		//case-1
+		if (n1 > n2) {
+			System.out.println("큰 숫자 : " + n1 + ", 작은 숫자 : " + n2);
+		} else {
+			System.out.println("큰 숫자 : " + n2 + ", 작은 숫자 : " + n1);
+		}
+		
+		//case-2
+		//if 키워드 한개만 이용, 추가적인 변수 선언이 필요
+		//변수선언
+		int temp = 0;
+		if(		) {
+			//1
+			
+			//2
+			
+			//3
+			
+		}
+		
+		System.out.println("큰 숫자 : " + n1 + ", 작은 숫자 : " + n2);
+		
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
