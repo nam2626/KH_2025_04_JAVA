689543f1620cf501c0de262f29a1b064eeba34a7
nam2626 <nam2626@naver.com>
2025-04-08 16:40:29 +0900

    while 예제

---
+++ Step04/src/E06_Quest.java
@@ -0,0 +1,23 @@
+
+public class E06_Quest {
+	/*
+	 * 	1~100까지 합을 출력
+	 * 
+	 * 	5050
+	 * 
+	 */
+	public static void main(String[] args) {
+		int i = 1, total = 0;
+				
+		while(i <= 100) {
+//			System.out.println(i);
+			total += i;
+			i++;
+		}
+		System.out.println(total);
+	}
+
+}
+
+
+
