3df41945b109d10b4568f313ff9bd9aa17e401b6
nam2626 <nam2626@naver.com>
2025-04-09 17:04:56 +0900

    do - while

---
+++ Step04/src/E22_do_while.java
@@ -0,0 +1,22 @@
+
+public class E22_do_while {
+	/*
+	 * 	do-while 반복문 선 작업 후 조건으로 반복문 실행
+	 * 	while/for문은 선 조건 처리 후 작업 실행
+	 * 
+	 * 	do-while은 한번은 반복을 해야할때 사용한다.
+	 * 
+	 */
+	public static void main(String[] args) {
+		int i = 10;
+		
+		do {
+			System.out.println("Hello World");
+			i++;
+		}while(i<5);
+	}
+
+}
+
+
+
