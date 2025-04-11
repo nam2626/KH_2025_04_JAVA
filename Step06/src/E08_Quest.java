84ec5c322aa85166bf67fe61ecdc9433bc758c56
nam2626 <nam2626@naver.com>
2025-04-11 15:21:46 +0900

    배열 예제

--- Step06/src/E08_Quest.java
+++ Step06/src/E08_Quest.java
@@ -2,8 +2,17 @@
 public class E08_Quest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String str = "Hello World";
+		char[] arr = str.toCharArray();
+		
+		//arr 배열에 있는 문자들을 전부 대문자로 변환해서 출력
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i] >= 'a' && arr[i] <= 'z')
+				arr[i] -= 32;
+		}
+		
+		str = new String(arr);
+		System.out.println(str);
 	}
 
 }
