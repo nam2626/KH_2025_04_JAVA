81f91f50923544e8d450a0ae5d5b034bd43fe81f
nam2626 <nam2626@naver.com>
2025-04-15 13:59:00 +0900

    2차원 배열

---
+++ Step06/src/E26_Array2D.java
@@ -0,0 +1,15 @@
+
+public class E26_Array2D {
+
+	public static void main(String[] args) {
+		//정수형 배열 3행 4열짜리 생성
+		//정수형 길이가 4인 배열을 3개 생성
+		int[][] arr = new int[3][4];
+		
+		System.out.println("행 개수 : "+arr.length);
+		System.out.println("0번 행의 개수 : " + arr[0].length);
+		System.out.println("1번 행의 개수 : " + arr[1].length);
+		System.out.println("2번 행의 개수 : " + arr[2].length);
+	}
+
+}
