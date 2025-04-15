51c0e67fc546ad19ec113047d14150c5f11a7ba8
nam2626 <nam2626@naver.com>
2025-04-15 17:08:25 +0900

    ㄱ 자 채우기 수정

--- Step06/src/E31_Quest.java
+++ Step06/src/E31_Quest.java
@@ -3,19 +3,24 @@ public class E31_Quest {
 	//달팽이 채우기
 	public static void main(String[] args) {
 		int[][] arr = new int[5][5];
-		int row = 0, col = 0, n = 1;
+		// row : 행번호, col : 열번호, n : 채울값
+		// sw : 증감값, k : 채울값의 개수
+		int row = 0, col = -1, n = 1, sw = 1, k = 5;
 		
 		//가로방향 채움
-		for( ;col < 5;col++ ) {
+		for(int i = 0 ;i < k; i++ ) {
+			col += sw;
+			arr[row][col] = n++;
+		}
+		k--;
+		
+		//세로방향 채움
+		for(int i = 0 ;i < k; i++ ) {
+			row += sw;
 			arr[row][col] = n++;
 		}
 		
-		col--;
-		row++;
-		//세로방향 채움
-		for( ;row < 5; row++) {
-			arr[row][col] = n++;
-		}
+		sw = -sw;
 		
 		for (int i = 0; i < arr.length; i++) {
 			for (int c = 0; c < arr[i].length; c++) {
