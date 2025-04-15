95caa8e918c6c447fd49f4a1c71bb75eed394197
nam2626 <nam2626@naver.com>
2025-04-15 17:15:14 +0900

    달팽이 채우기

--- Step06/src/E31_Quest.java
+++ Step06/src/E31_Quest.java
@@ -6,22 +6,24 @@ public class E31_Quest {
 		// row : 행번호, col : 열번호, n : 채울값
 		// sw : 증감값, k : 채울값의 개수
 		int row = 0, col = -1, n = 1, sw = 1, k = 5;
-		
-		//가로방향 채움
-		for(int i = 0 ;i < k; i++ ) {
-			col += sw;
-			arr[row][col] = n++;
+
+		while(true) {
+			//가로방향 채움
+			for(int i = 0 ;i < k; i++ ) {
+				col += sw;
+				arr[row][col] = n++;
+			}
+			k--;
+			//채우는 개수가 0이면 종료
+			if(k == 0) break;
+			
+			//세로방향 채움
+			for(int i = 0 ;i < k; i++ ) {
+				row += sw;
+				arr[row][col] = n++;
+			}
+			sw = -sw;
 		}
-		k--;
-		
-		//세로방향 채움
-		for(int i = 0 ;i < k; i++ ) {
-			row += sw;
-			arr[row][col] = n++;
-		}
-		
-		sw = -sw;
-		
 		for (int i = 0; i < arr.length; i++) {
 			for (int c = 0; c < arr[i].length; c++) {
 				System.out.printf("%2d ",arr[i][c]);
