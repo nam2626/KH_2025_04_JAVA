310cb9bdb3438f5838ef4cc7c527e11df0fd3311
nam2626 <nam2626@naver.com>
2025-04-18 16:43:59 +0900

    삽입 정렬

--- Step07/src/e03_method/E05_SortArray.java
+++ Step07/src/e03_method/E05_SortArray.java
@@ -1,5 +1,7 @@
 package e03_method;
 
+import java.util.Scanner;
+
 public class E05_SortArray {
 
 	//배열 내용을 일렬로 출력하는 메서드
@@ -45,12 +47,30 @@ public class E05_SortArray {
 			printArray(arr);
 		}
 	}
+	
+	public static void insertSort() {
+		int[] arr = new int[5];
+		Scanner sc = new Scanner(System.in);
+		
+		//배열 첫번째 요소부터 마지막 요소까지 숫자 입력받아서 저장
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print("숫자 입력 : ");
+			arr[i] = sc.nextInt();
+			//삽입 정렬 수행
+			
+			printArray(arr);
+		}
+	}
+	
 	public static void main(String[] args) {
 		int[] arr1 = new int[] {5,7,9,3,1};
 		sortBuble(arr1);
 		System.out.println();
 		int[] arr2 = new int[] {5,7,9,3,1};
 		sortSelect(arr2);
+		System.out.println();
+		
+		insertSort();
 	}
 
 }
