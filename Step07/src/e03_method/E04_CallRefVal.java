4d72981c9c87269d8e2ab8cd49a43492f4056119
nam2626 <nam2626@naver.com>
2025-04-18 10:47:52 +0900

    참조에 의한 호출

--- Step07/src/e03_method/E04_CallRefVal.java
+++ Step07/src/e03_method/E04_CallRefVal.java
@@ -13,13 +13,24 @@ public class E04_CallRefVal {
 		n++;
 		return n;
 	}
-	
+	/*
+	 * 	Call by Reference - 참조에 의한 호출 --> 메모리 주소값을 복사해서 전달
+	 * 		메모리 주소값을 전달하기 떄문에 다른 메서드에서 수정 작업을 하면 원본 내용이 바뀐다.
+	 * 		전달 받은 메모리 주소에 원본값이 있기때문에 다른 메서드에서 동일한 데이터에 접근함.
+	 * 		주로 참조형 변수타입에서 발생(클래스, ...)
+	 */
+	static void updateStudent(Student std) {
+		std.updateScore(4.5);
+	}
 	public static void main(String[] args) {
 		int n = 10;
 		addNumber(n);
 		System.out.println(n);
 
 		Student std = new Student("1111","김철수","컴퓨터공학과",2.3);
+		std.printStudentInfo();
+		updateStudent(std);
+		std.printStudentInfo();
 		
 	}
 
