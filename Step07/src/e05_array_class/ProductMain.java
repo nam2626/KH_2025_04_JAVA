85aea44c5e560b52432143d6c8822ff048a1c848
nam2626 <nam2626@naver.com>
2025-04-21 10:19:50 +0900

    클래스 배열 예제

--- Step07/src/e05_array_class/ProductMain.java
+++ Step07/src/e05_array_class/ProductMain.java
@@ -3,7 +3,20 @@ package e05_array_class;
 public class ProductMain {
 
 	public static void main(String[] args) {
+		Product[] arr = new Product[3];
 		
+		arr[0] = new Product("아이폰15", 160);
+		arr[1] = new Product("갤럭시S25", 145);
+		arr[2] = new Product("LG OLED TV", 300);
+		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		
+		for(Product p : arr) {
+			System.out.println(p);
+		}
 	}
 
 }
+
