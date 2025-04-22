467e36e8fe0f68bcaa7e4ba5d72fd3dae3cbbe7a
nam2626 <nam2626@naver.com>
2025-04-22 10:52:28 +0900

    로또번호 생성기

--- Step08/src/e02_random/Lotto.java
+++ Step08/src/e02_random/Lotto.java
@@ -5,34 +5,46 @@ import java.util.Random;
 public class Lotto {
 	public static boolean isDuplicateNumber(int[] arr, int n) {
 		for (int e : arr) {
-			if(e == n)
+			if (e == n)
 				return true;
 		}
 		return false;
 	}
-	
+
 	public static int[] generateLotto() {
-		//배열 생성
+		// 배열 생성
 		int[] arr = new int[6];
-		
-		for(int i=0;i<arr.length;i++) {
-			//숫자 랜덤으로 뽑음
-			int n = (new Random()).nextInt(1, 46);
-			//중복 체크
-			if(isDuplicateNumber(arr, n)) {
-				i--;
-				continue;
-			}
-			//숫자 배열에 저장
+
+		for (int i = 0; i < arr.length; i++) {
+			// 숫자 랜덤으로 뽑음
+//			int n = (new Random()).nextInt(1, 46);
+			// 중복 체크
+//			while (isDuplicateNumber(arr, n)) {
+//				n = (new Random()).nextInt(1, 46);
+//			}
+			int n = 0;
+			while (isDuplicateNumber(arr, (n = (new Random()).nextInt(1, 46)))) ;
+			// 숫자 배열에 저장
 			arr[i] = n;
 		}
-		
-		//배열 리턴
+
+		// 배열 리턴
 		return arr;
 	}
-	
+
+	public static void printArray(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.printf("%2d ", arr[i]);
+		}
+		System.out.println();
+	}
+
 	public static void main(String[] args) {
-		
+		for (int i = 0; i < 5; i++) 
+			printArray(generateLotto());
 	}
 
 }
+
+
+
