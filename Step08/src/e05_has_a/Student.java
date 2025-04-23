798c588a70439ec11389778cd792a5e78858c018
nam2626 <nam2626@naver.com>
2025-04-23 16:09:02 +0900

    평점 평균 및 등급

--- Step08/src/e05_has_a/Student.java
+++ Step08/src/e05_has_a/Student.java
@@ -24,10 +24,43 @@ public class Student {
 	//과목리스트에 저장된 과목들의 평점 평균과 등급을 아래와 같은 형태로 리턴
 	//	4.3 / A
 	public String getGrade() {
+		//평점의 평균을 구하기
+		double avg = 0.0;
 		for(Subject sub : subList) {
-			
+			avg += sub.getGradeScore();
 		}
-		return "";
+		avg /= subList.size();
+		
+		String grade = "F";
+		switch((int)(avg * 10) / 5) {
+		case 9:
+			grade = "A+";
+			break;
+		case 8:
+			grade = "A";
+			break;
+		case 7:
+			grade = "B+";
+			break;
+		case 6:
+			grade = "B";
+			break;
+		case 5:
+			grade = "C+";
+			break;
+		case 4:
+			grade = "C";
+			break;
+		case 3:
+			grade = "D+";
+			break;
+		case 2:
+			grade = "D";
+			break;
+		
+		}
+		
+		return avg + " / " + grade ;
 	}
 }
 
