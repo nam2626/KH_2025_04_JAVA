aa82b94593e197bc81d6dab55e6ee525cbbf737b
nam2626 <nam2626@naver.com>
2025-04-24 13:52:40 +0900

    CarMain

---
+++ Step09/src/e03_super/Car.java
@@ -0,0 +1,38 @@
+package e03_super;
+
+public class Car {
+	protected int fuel;
+	protected int speed;
+	
+	public Car() {
+		fuel = 100;
+	}
+	
+	public void accerate() {
+		if(fuel < 1) {
+			System.out.println("연료를 채워 주세요.");
+			return;
+		}
+
+		if(speed + 5 <= 240)
+			speed += 5;
+		System.out.println("엑셀을 밟습니다.");
+		System.out.println(
+				String.format("현재 속도 : %dkm/h - 남은 연료 : %d",
+						speed, fuel));
+	}
+	
+	public void brake() {
+		if(speed <= 10) speed = 0;
+		else speed -= 10;
+		System.out.println("브레이크를 밟습니다.");
+		System.out.println(
+				String.format("현재 속도 : %dkm/h - 남은 연료 : %d",
+						speed, fuel));
+	}
+		
+}
+
+
+
+
