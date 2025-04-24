44ea7af529208065f80f8101007351bd298f1b92
nam2626 <nam2626@naver.com>
2025-04-24 14:06:32 +0900

    super 예제

---
+++ Step09/src/e03_super/AvanteN.java
@@ -0,0 +1,32 @@
+package e03_super;
+
+public class AvanteN extends Avante{
+	private boolean driveMode;//false - 일반, true - 고속 주행
+	
+	public void changeMode() {
+		driveMode = !driveMode;
+		
+		if(driveMode)
+			System.out.println("고속 주행 모드로 전환합니다.");
+		else
+			System.out.println("일반 주행 모드로 전환합니다.");
+	}
+	
+	@Override
+	public void accerate() {
+		//고속 주행 모드면 속도는 15 증가, 연료량 3씩 소모
+		if(fuel < 3) {
+			System.out.println("연료를 채워 주세요.");
+			return;
+		}
+
+		if(speed + 15 <= 240)
+			speed += 15;
+		fuel -= 3;
+		System.out.println("엑셀을 밟습니다.");
+		System.out.println(
+				String.format("현재 속도 : %dkm/h - 남은 연료 : %d",
+						speed, fuel));
+	}
+	
+}
