a085cf8e9c678ab84743b56fe99964f3dfd2cb82
nam2626 <nam2626@naver.com>
2025-04-24 14:18:58 +0900

    super 예제

--- Step09/src/e03_super/AvanteN.java
+++ Step09/src/e03_super/AvanteN.java
@@ -14,19 +14,23 @@ public class AvanteN extends Avante{
 	
 	@Override
 	public void accerate() {
-		//고속 주행 모드면 속도는 15 증가, 연료량 3씩 소모
-		if(fuel < 3) {
-			System.out.println("연료를 채워 주세요.");
-			return;
+		if(driveMode) {
+			//고속 주행 모드면 속도는 15 증가, 연료량 3씩 소모
+			if(fuel < 3) {
+				System.out.println("연료를 채워 주세요.");
+				return;
+			}
+	
+			if(speed + 15 <= 240)
+				speed += 15;
+			fuel -= 3;
+			System.out.println("엑셀을 밟습니다.");
+			System.out.println(
+					String.format("현재 속도 : %dkm/h - 남은 연료 : %d",
+							speed, fuel));
+		}else {
+			super.accerate();
 		}
-
-		if(speed + 15 <= 240)
-			speed += 15;
-		fuel -= 3;
-		System.out.println("엑셀을 밟습니다.");
-		System.out.println(
-				String.format("현재 속도 : %dkm/h - 남은 연료 : %d",
-						speed, fuel));
 	}
 	
 }
--- Step09/src/e03_super/CarMain.java
+++ Step09/src/e03_super/CarMain.java
@@ -3,12 +3,17 @@ package e03_super;
 public class CarMain {
 
 	public static void main(String[] args) {
-		Avante car = new Avante();
+		AvanteN car = new AvanteN();
 		
-		for(int i=0;i<200;i++)
-			car.accerate();
-		for(int i=0;i<100;i++)
-			car.brake();
+		car.accerate();
+		car.accerate();
+		car.accerate();
+		
+		car.changeMode();
+		
+		car.accerate();
+		car.accerate();
+		car.accerate();
 		
 	}
 
