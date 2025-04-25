ca9a02bb7f123ae73bfb196df3ac4b971274e6b8
nam2626 <nam2626@naver.com>
2025-04-25 15:39:35 +0900

    hashCode, toString 예제

---
+++ Step10/src/e01_object/PersonMain.java
@@ -0,0 +1,20 @@
+package e01_object;
+
+public class PersonMain {
+	public static void main(String[] args) {
+		Person p = new Person("홍길동", 20);
+		Person s = new Person("홍길동", 20);
+		
+		System.out.println(p.hashCode());
+		System.out.println(System.identityHashCode(p));
+		System.out.println(s.hashCode());
+		//내용은 같지만 메모리 주소가 달라서 false가 나옴
+		System.out.println(p == s);
+		
+		
+	}
+}
+
+
+
+
