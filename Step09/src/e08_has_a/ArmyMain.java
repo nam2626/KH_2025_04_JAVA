2716788f5d9fecc53cc5f0361f0f17c86ee49629
nam2626 <nam2626@naver.com>
2025-04-25 12:05:53 +0900

    테스트

---
+++ Step09/src/e08_has_a/ArmyMain.java
@@ -0,0 +1,13 @@
+package e08_has_a;
+
+public class ArmyMain {
+
+	public static void main(String[] args) {
+		Gun gun = new Gun();
+		Army army = new Army(gun);
+
+		for (int i = 0; i < 20; i++)
+			army.shot();
+	}
+
+}
