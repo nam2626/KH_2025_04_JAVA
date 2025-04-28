d23da19881d49b8c95b5233cbe1ced95c2b0d2f2
nam2626 <nam2626@naver.com>
2025-04-28 10:20:14 +0900

    추상 클래스

---
+++ Step10/src/e02_abstract/Animal.java
@@ -0,0 +1,15 @@
+package e02_abstract;
+
+public class Animal {
+
+	public Animal() {
+		System.out.println("Animal 생성자");
+	}
+	
+	public void eat() {
+		System.out.println("동물이 먹이를 먹습니다.");
+	}
+	
+	public void run() ;
+	
+}
---
+++ Step10/src/e02_abstract/Dog.java
@@ -0,0 +1,5 @@
+package e02_abstract;
+
+public class Dog extends Animal{
+	
+}
