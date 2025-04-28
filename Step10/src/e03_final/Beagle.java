d5d6c9edafec877b5052ae6542498513354b90f2
nam2626 <nam2626@naver.com>
2025-04-28 11:41:42 +0900

    final 클래스, 메서드

---
+++ Step10/src/e03_final/Beagle.java
@@ -0,0 +1,6 @@
+package e03_final;
+//final 클래스는 상속을 받을 수 없음
+//public class Beagle extends Dog{
+public class Beagle {
+	
+}
---
+++ Step10/src/e03_final/Dog.java
@@ -0,0 +1,7 @@
+package e03_final;
+
+public final class Dog {
+	public void eat() {
+		System.out.println("개가 사료를 먹습니다.");
+	}
+}
