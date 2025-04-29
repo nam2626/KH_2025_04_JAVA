da04d3a1ddb54b7fab75068fd3724247b727e225
nam2626 <nam2626@naver.com>
2025-04-29 12:01:01 +0900

    마우스 extends 예제

--- Step11/src/e02_generic_extends/MouseMain.java
+++ Step11/src/e02_generic_extends/MouseMain.java
@@ -3,8 +3,19 @@ package e02_generic_extends;
 public class MouseMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		GamingMouse gamingMouse = new GamingMouse();
+		OfficeMouse officeMouse = new OfficeMouse();
+		
+//		Office 마우스는 Mouse를 구현하지 않아서 제네릭 지정하는 타입으로 못씀
+//		MouseHUB<OfficeMouse> hub = 
+//				new MouseHUB<OfficeMouse>(officeMouse);
+		
+		MouseHUB<GamingMouse> hub = 
+				new MouseHUB<GamingMouse>(gamingMouse);
+		hub.click(1);
+		hub.click(3);
+		hub.click(2);
+		hub.wheel();
 	}
 
 }
