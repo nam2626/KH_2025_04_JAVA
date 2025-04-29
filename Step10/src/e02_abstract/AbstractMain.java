55d216c3227a18e68033b8d24ad7733e30c48d42
nam2626 <nam2626@naver.com>
2025-04-29 09:45:11 +0900

    추상 클래스 설명 추가

--- Step10/src/e02_abstract/AbstractMain.java
+++ Step10/src/e02_abstract/AbstractMain.java
@@ -5,6 +5,7 @@ public class AbstractMain {
 	public static void main(String[] args) {
 		//추상 클래스는 직접적인 생성이 되지 않음.
 //		Animal a = new Animal();
+		//자식 클래스가 생성시 이때만 간접적으로 생성됨
 		Dog d = new Dog();
 		d.run();
 		//추상 클래스도 데이터 타입, 자식 클래스를 형변환 시켜서 저장 작업은 됨
