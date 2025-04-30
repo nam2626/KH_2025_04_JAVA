d4b60d298845538b9278c20c1c29e56767d1fc4a
nam2626 <nam2626@naver.com>
2025-04-30 16:15:46 +0900

    Inner 클래스

---
+++ Step12/src/e03_inner/OutterClass.java
@@ -0,0 +1,30 @@
+package e03_inner;
+
+public class OutterClass {
+	private int outter;
+
+	public OutterClass(int outter) {
+		this.outter = outter;
+	}
+	
+	public void printOutter() {
+		System.out.println("outter : " + outter);
+	}
+	
+	public class Inner{
+		private int inner;
+
+		public Inner(int inner) {
+			this.inner = inner;
+		}
+		
+		public int sum() {
+			return outter + inner;
+		}
+		
+		public void printInner() {
+			System.out.println(outter + " -> " + inner);
+		}
+		
+	}
+}
