aa605050edb50b356a7f42e1248502e630bfdc52
nam2626 <nam2626@naver.com>
2025-05-01 10:06:21 +0900

    클릭 이벤트 처리 방법

---
+++ Step12/src/e05_event/View.java
@@ -0,0 +1,27 @@
+package e05_event;
+
+public class View {
+	protected String name;
+	private OnClickListener clickListener;
+	
+	public View(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void onClick() {
+		
+	}
+	
+	public static interface OnClickListener{
+		public void onClick(View view);
+	}
+	
+}
+
+
+
+
