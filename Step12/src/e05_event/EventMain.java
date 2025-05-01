9948fa20895874ef45fab39e1f82d407328c0493
nam2626 <nam2626@naver.com>
2025-05-01 10:24:25 +0900

    이벤트 처리 클래스

--- Step12/src/e05_event/EventMain.java
+++ Step12/src/e05_event/EventMain.java
@@ -1,10 +1,28 @@
 package e05_event;
 
+
 public class EventMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Button btnRegister = new Button("회원가입");
+		btnRegister.setClickListener(
+				new ButtonRegisterOnClickListener());
+		btnRegister.onClick();
+		
+		String id = "admin";
+		Button btnLogin = new Button("로그인");
+		btnLogin.setClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				System.out.println(view.getName() + " 버튼 클릭");
+				//외부에 있는 지역변수나 필드도 바로 접근이 가능
+				System.out.println(id + " 로그인 처리");
+			}
+		});
+		btnLogin.onClick();
 	}
 
 }
+
+
+
